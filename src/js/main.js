aabd9e0a62ea15de41a99317732a5350ae7703c1
Germán Sánchez <germands2013@gmail.com>
2023-06-01 15:41:29 -0300

    Update deck length

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,10 @@ import cards from "./cards.js";
 //--------flip card on click-----------
 const htmlCard = document.querySelector(".card");
 const htmlCardBack = document.querySelector(".card");
-
+//define deck length
+const deckLength = 52;
 //-------generate random card---------
-let randomCard = Math.floor(Math.random() * cards.length);
+let randomCard = Math.floor(Math.random() * deckLength);
 
 // -------insert initial card on html---------
 let cardContent = `<use class="card" href="svg-cards.svg#${cards[randomCard].name}" x="0" y="0" />`;
@@ -15,7 +16,7 @@ cardFront.innerHTML = cardContent;
 htmlCardBack.addEventListener("click", function () {
   if (htmlCard.classList.contains("flipped")) {
     //-------generate random card---------
-    randomCard = Math.floor(Math.random() * cards.length);
+    randomCard = Math.floor(Math.random() * deckLength);
 
     // -------insert card on html---------
     cardContent = `<use class="card" href="svg-cards.svg#${cards[randomCard].name}" x="0" y="0" />`;
