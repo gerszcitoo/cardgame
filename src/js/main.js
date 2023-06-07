d16e748fc9bb31f1868557d93e69cec1737cf419
German Sanchez <germands2013>
2023-06-07 15:50:22 -0300

    made deck behid the main card

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,35 @@
 import cards from "./cards.js";
 
-//--------flip card on click-----------
-const htmlCard = document.querySelector(".card");
-const htmlCardBack = document.querySelector(".card");
+//--------get card suit----------
+function getCardSuit(cardNumber) {
+  // array with every card suit
+  const cardSuits = cards.map((card) => card.name.split("_")[0]);
+  return cardSuits[cardNumber];
+}
 
-//-------generate random card---------
-let randomCard = Math.floor(Math.random() * cards.length);
+function flipCardOnClick() {
+  //--------flip card on click-----------
+  const htmlCard = document.querySelector(".card");
 
-// -------insert initial card on html---------
-let cardContent = `<use class="card" href="svg-cards.svg#${cards[randomCard].name}" x="0" y="0" />`;
-let cardFront = document.querySelector(".cardSvg");
-cardFront.innerHTML = cardContent;
+  //-------generate random card---------
+  let randomCardNumber = Math.floor(Math.random() * 52);
 
-htmlCardBack.addEventListener("click", function () {
-  if (htmlCard.classList.contains("flipped")) {
-    //-------generate random card---------
-    randomCard = Math.floor(Math.random() * cards.length);
+  // -------insert initial card on html---------
+  let cardContent = `<use class="card" href="svg-cards.svg#${cards[randomCardNumber].name}" x="0" y="0" />`;
+  let cardFront = document.querySelector(".cardSvg");
+  cardFront.innerHTML = cardContent;
 
-    // -------insert card on html---------
-    cardContent = `<use class="card" href="svg-cards.svg#${cards[randomCard].name}" x="0" y="0" />`;
-    cardFront.innerHTML = cardContent;
-  }
-  htmlCard.classList.toggle("flipped");
-});
+  htmlCard.addEventListener("click", function () {
+    if (htmlCard.classList.contains("flipped")) {
+      //-------generate random card and get its suit---------
+      randomCardNumber = Math.floor(Math.random() * 52);
+      let cardSuit = getCardSuit(randomCardNumber);
+      // -------insert card on html---------
+      cardContent = `<use class="card" href="svg-cards.svg#${cards[randomCardNumber].name}" x="0" y="0" />`;
+      cardFront.innerHTML = cardContent;
+    }
+    htmlCard.classList.toggle("flipped");
+  });
+}
+
+window.onload = flipCardOnClick();
