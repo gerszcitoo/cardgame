8748fd9c2b64a8fe245f937152fe65dbbffd7b04
German Sanchez <germands2013>
2023-06-08 18:37:35 -0300

    send card to back of the deck animation WIP

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
 import cards from "./cards.js";
 
+// DESARROLLO:
+// AL HACER CLICK EN LA CARTA FRONTAL, SE HACE LA ANIMACIÓN DE ENVIARLA ATRÁS,
+// A LA VEZ SE AÑADE UNA CARTA ATRÁS/ADELANTE DE TODO DEL MAZO Y SE MODIFICA LA POSICIÓN DEL MAZO
+// DESPUÉS LA CARTA NUEVA SE LE DA EL FUNCIONAMIENTO DEL BACK DE LA CARTA
+// Y AL CLICKEAR VUELVE AL FUNCIONAMIENTO DE LA CARTA ORIGINAL
+// -----------------------IDEA FRENTE FALSO-----------------------
+// TENER DOS CARTAS, UNA FLIPEABLE Y UNA QUE SÓLO ES EL FRENTE FALSO
+// AL HACER CLICK EN EL FRENTE DE LA CARTA ORIGINAL, HACER LA ANIMACIÓN CON EL FRENTE FALSO,
+// AL TERMINAR ANIMACIÓN, MOSTRAR NUEVAMENTE CARTA ORIGINAL PERO DEL REVERSO
+
 //--------get card suit----------
 function getCardSuit(cardNumber) {
   // array with every card suit
@@ -16,8 +26,11 @@ function flipCardOnClick() {
 
   // -------insert initial card on html---------
   let cardContent = `<use class="card" href="svg-cards.svg#${cards[randomCardNumber].name}" x="0" y="0" />`;
-  let cardFront = document.querySelector(".cardSvg");
+  const cardFront = document.querySelector(".cardSvg");
+  const fakeFrontSvg = document.querySelector(".fakeFrontSvg");
+
   cardFront.innerHTML = cardContent;
+  fakeFrontSvg.innerHTML = cardContent;
 
   htmlCard.addEventListener("click", function () {
     if (htmlCard.classList.contains("flipped")) {
@@ -27,8 +40,22 @@ function flipCardOnClick() {
       // -------insert card on html---------
       cardContent = `<use class="card" href="svg-cards.svg#${cards[randomCardNumber].name}" x="0" y="0" />`;
       cardFront.innerHTML = cardContent;
+      fakeFrontSvg.innerHTML = cardContent;
+    } else {
+      cardFront.classList.toggle("hidingFront");
+      fakeFrontSvg.classList.toggle("flip");
+      cardFront.style.visibility = "hidden";
+      setTimeout(() => {
+        cardFront.classList.toggle("hidingFront");
+        fakeFrontSvg.classList.toggle("flip");
+        cardFront.style.visibility = "visible";
+      }, 1000);
     }
     htmlCard.classList.toggle("flipped");
+    /*     setTimeout(() => {
+      htmlCard.style.visibility = "hidden";
+    }, 800); */
+    // htmlCard.classList.toggle("slide-to-bottom");
   });
 }
 
